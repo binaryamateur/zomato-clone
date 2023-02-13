29e5eb290e101b382ddb2a09810467d6262292a7
Souvik Patra <souvikpatra@souvik.patra-C02DT3PXMD6N>
2023-02-13 15:12:02 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './styles/main.css';
+
+const app = document.createElement('div');
+app.className = "app";
+app.innerHTML = "Hello World";
+document.body.append(app);
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+module.exports = {
+ "mode": "none",
+ "entry": "./src/index.js",
+ "output": {
+   "path": __dirname + '/dist',
+   "filename": "bundle.js"
+ },
+devServer: {
+    static: path.join(__dirname, 'dist')
+ },
+ "module": {
+    "rules": [
+      {
+        "test": /\.css$/,
+        "use": [
+          "style-loader",
+          "css-loader"
+        ]
+      },
+    ]
+  }
+}

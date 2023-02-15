08b1c06c71a49bf9a73ff34a70956a11b25f04fa
Souvik Patra <souvikpatra@souvik.patra-C02DT3PXMD6N>
2023-02-15 19:08:31 +0530

    corrected sticky header

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,19 @@
-const main = document.createElement('div');
-main.innerHTML = "Main";
+import styles from "./styles/main.module.css";
 
-export {main};
+class Main{
+    constructor(){
+        this.content = document.createElement("div");
+        this.content.className = styles.main;
+        this.content.classList.add(styles["max-width-wrapper"]);
+        this.content.innerHTML = `
+            <div class = "${styles.heading}">Delivery Restaurants in Thakur Wara, Sohna, India</div>
+        `;
+    }
+
+    get(){
+        return this.content;
+    }
+
+}
+
+export {Main};
--- src/Rest.js
+++ src/Rest.js
@@ -1,3 +1,4 @@
+import { Main } from './Main';
 import { StickyHeader } from './StickyHeader';
 import styles from "./styles/rest.module.css";
 import { SubHeader } from './SubHeader';
@@ -8,9 +9,11 @@ class Rest {
         this.content.className = styles.rest;
         const stickyHeader = new StickyHeader(["Filters", "Delivery Time", "Rating: 4.0+", "Pure Veg", "Cuisines", "More Filters"]);
         const subHeader = new SubHeader();
+        const main = new Main();
         console.log(subHeader);
         this.content.append(stickyHeader.get());
         this.content.append(subHeader.get());
+        this.content.append(main.get());
         // this.content.innerHTML = `
         //     ${stickyHeader.get().outerHTML}
         //     ${subHeader.get().outerHTML}

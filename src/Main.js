a06287a44497e715c4ba4f763d5e42ca28acaa15
Souvik Patra <souvikpatra@souvik.patra-C02DT3PXMD6N>
2023-02-16 19:45:10 +0530

    made fluid grid

--- src/Main.js
+++ src/Main.js
@@ -5,7 +5,6 @@ import styles from "./styles/main.module.css";
 class Main{
     constructor(){
         this.content = document.createElement("div");
-        this.content.className = styles.main;
         this.content.classList.add(styles["max-width-wrapper"]);
         this.content.innerHTML = `
             <div class = "${styles.heading}">Delivery Restaurants in Thakur Wara, Sohna, India</div>
